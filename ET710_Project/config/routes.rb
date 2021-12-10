c40f131b05d3e499368ae467f43b44538ac53476
EC2 Default User <ec2-user@ip-172-31-20-226.ec2.internal>
2021-12-10 23:59:57 +0000

    Attempt 12-10

---
+++ ET710_Project/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/index'
+  get 'welcome/p2'
+  get 'welcome/p3'
+  get 'welcome/p4'
+  get 'welcome/p5'
+  get 'welcome/p6'
+  get 'welcome/p7'
+  get 'welcome/p8'
+  get 'welcome/p9'
+  get 'welcome/p10'
+  get 'welcome/p11'
+  get 'welcome/p12'
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
